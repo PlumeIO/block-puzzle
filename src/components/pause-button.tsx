8e04b7d5f93f768b3d85547e4c7a2bbdfa1d43ba
Aditya81016 <aditya.jaiswal.81016@gmail.com>
2024-07-07 16:35:01 +0530

    feat: :sparkles: Create pause button
    Created a new pause-button component in src/components
    
    Resolved issue #7: Create menu button component

---
+++ src/components/pause-button.tsx
@@ -0,0 +1,47 @@
+import { Home, Pause } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+
+function MenuButton() {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          className="fixed top-4 right-4"
+          variant={"outline"}
+          size={"icon-lg"}
+        >
+          <Pause />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="max-md:max-w-[90vw] rounded-md">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Game Paused</AlertDialogTitle>
+          <AlertDialogDescription className="hidden">
+            Pause Menu
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <Link to={"/"} className="w-full">
+          <Button className="w-full">
+            <Home /> | Go back home
+          </Button>
+        </Link>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Close</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export default MenuButton;
