388ea80fcdb931f223a5da9684711d25b253b1a6
Aditya81016 <aditya.jaiswal.81016@gmail.com>
2024-07-09 21:32:44 +0530

    feat: :sparkles: Make blocks from block-menu dragable
    Added an extra block in block menu i.e. the block being dragged
    
    Resolved issue #9: Make blocks from block menu dragable

--- src/components/block-menu.tsx
+++ src/components/block-menu.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Block from "./block";
 
 export interface BlockMenuProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -7,25 +7,80 @@ export interface BlockMenuProps extends React.HTMLAttributes<HTMLDivElement> {
     grid: Array<0 | 1>[];
     color: string;
   }[];
+  hoverSize?: number;
+}
+
+type BlockType = BlockMenuProps["blocks"][number];
+interface DraggedBlock extends BlockType {
+  id: number;
 }
 
 const BlockMenu = React.forwardRef<HTMLDivElement, BlockMenuProps>(
-  ({ className, blocks, ...props }, ref) => {
+  ({ className, blocks, hoverSize = 48, ...props }, ref) => {
+    const [draggedBlock, setDraggedBlock] = useState<DraggedBlock | undefined>(
+      undefined
+    );
+
+    const [dragPos, setDragPos] = useState<[number, number] | undefined>(
+      undefined
+    );
+
+    useEffect(() => {
+      const handleTouchMove = (e: TouchEvent) => {
+        setDragPos([e.touches[0].clientX, e.touches[0].clientY]);
+      };
+
+      document.addEventListener("touchmove", handleTouchMove);
+
+      return () => {
+        document.removeEventListener("touchmove", handleTouchMove);
+      };
+    });
+
     return (
-      <div
-        className={cn(
-          className,
-          "w-full flex items-center justify-evenly h-20 bg-secondary rounded-md"
+      <>
+        <div
+          className={cn(
+            className,
+            "w-full flex items-center justify-evenly h-20 bg-secondary rounded-md"
+          )}
+          ref={ref}
+          {...props}
+        >
+          {blocks.map((block, i) => (
+            <div
+              className="w-16 h-16 flex items-center justify-center"
+              onTouchStart={(e) => {
+                setDraggedBlock({ ...block, id: i });
+                setDragPos([e.touches[0].clientX, e.touches[0].clientY]);
+              }}
+              onTouchEnd={() => {
+                setDraggedBlock(undefined);
+                setDragPos(undefined);
+              }}
+              key={i}
+            >
+              <Block
+                cellSize={12}
+                {...block}
+                className={draggedBlock?.id === i ? "invisible" : "visible"}
+              />
+            </div>
+          ))}
+        </div>
+        {draggedBlock && dragPos && (
+          <Block
+            cellSize={hoverSize}
+            style={{
+              left: dragPos[0] + "px",
+              top: dragPos[1] + "px",
+            }}
+            className="fixed -translate-x-1/2 -translate-y-1/2"
+            {...draggedBlock}
+            id={"block-menu-#" + draggedBlock.id}
+          />
         )}
-        ref={ref}
-        {...props}
-      >
-        {blocks.map((block, i) => (
-          <div className="w-16 h-16 flex items-center justify-center" key={i}>
-            <Block cellSize={12} {...block} />
-          </div>
-        ))}
-      </div>
+      </>
     );
   }
 );
