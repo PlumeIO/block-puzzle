b5701376b2b932e57f79e073cfac057f334c73ce
Aditya81016 <aditya.jaiswal.81016@gmail.com>
2024-08-03 09:43:39 +0530

    build: :rocket: Add android support using capacitor
    Resolved issue #23: Build a mobile app

---
+++ android/app/src/main/java/com/plumeapps/blockpuzzle/MainActivity.java
@@ -0,0 +1,5 @@
+package com.plumeapps.blockpuzzle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.plumeapps.blockpuzzle',
+  appName: 'Block Puzzle',
+  webDir: 'dist'
+};
+
+export default config;
